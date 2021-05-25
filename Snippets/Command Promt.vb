ee3e5ad6b2f8c349ec65f0c2b166db1b058b7d30
WOWZON3 <80276117+WOWZON3@users.noreply.github.com>
2021-05-25 16:50:58 +0200

    Update Command Promt.vb

--- Snippets/Command Promt.vb
+++ Snippets/Command Promt.vb
@@ -8,3 +8,11 @@ Public Class MyUtilities
         p.Start() 
     End Sub
 End Class
+
+
+Public Class Form1
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        Shell("cmd.exe /k" + TextBox1.Text)
+        Close()
+    End Sub
+End Class
